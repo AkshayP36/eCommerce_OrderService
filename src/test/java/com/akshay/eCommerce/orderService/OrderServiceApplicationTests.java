22859ac2a063ed8e6abe5506b62dfc74957ccc4d
Akshay Patil <54596061+AkshayP36@users.noreply.github.com>
2026-07-21 20:01:55 +0530

    Order service initialization code

---
+++ src/test/java/com/akshay/eCommerce/orderService/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshay.eCommerce.orderService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
